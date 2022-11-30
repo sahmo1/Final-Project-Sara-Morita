ddf4e5be731025d4fcde197c2351beabef006423
Sara Morita <sara.morita97@gmail.com>
2022-11-30 09:31:22 +0900

    repo almost done and check for errors

--- src/main/java/com/company/FinalProjectSaraMorita/repositories/GameRepository.java
+++ src/main/java/com/company/FinalProjectSaraMorita/repositories/GameRepository.java
@@ -1,4 +1,15 @@
 package com.company.FinalProjectSaraMorita.repositories;
 
-public class GameRepository {
+import com.company.FinalProjectSaraMorita.models.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public class GameRepository extends JpaRepository<Game, Integer> {
+
+    List<Game> findAllGamesByStudio(String studio);
+    List<Game> findAllGamesByTitle(String title);
+    List<Game> findAllGamesByEsrbRating(String esrbRating);
+
+
 }
--- src/main/java/com/company/FinalProjectSaraMorita/repositories/TshirtRepository.java
+++ src/main/java/com/company/FinalProjectSaraMorita/repositories/TshirtRepository.java
@@ -1,4 +1,14 @@
 package com.company.FinalProjectSaraMorita.repositories;
 
-public class TshirtRepository {
+import com.company.FinalProjectSaraMorita.models.Tshirt;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public class TshirtRepository extends JpaRepository<Tshirt, Integer> {
+
+    List<Tshirt> findTshirtByColor(String color);
+    List<Tshirt> findTshirtBySize(String size);
+
+
 }

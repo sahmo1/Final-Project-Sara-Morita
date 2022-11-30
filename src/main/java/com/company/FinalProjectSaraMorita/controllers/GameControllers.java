9ab58de57b0aadb664c19694319b5c4dc40c60c7
Sara Morita <sara.morita97@gmail.com>
2022-11-30 09:01:36 +0900

    controllers done

--- src/main/java/com/company/FinalProjectSaraMorita/controllers/GameControllers.java
+++ src/main/java/com/company/FinalProjectSaraMorita/controllers/GameControllers.java
@@ -1,4 +1,87 @@
 package com.company.FinalProjectSaraMorita.controllers;
 
+import com.company.FinalProjectSaraMorita.models.Game;
+import com.company.FinalProjectSaraMorita.repositories.GameRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class GameControllers {
+
+    @Autowired
+    GameRepository gameRepo;
+
+    @GetMapping("/games")
+    public List<Game> getGames(){
+        return gameRepo.findAll();
+    }
+
+
+    @GetMapping("/games/{id}")
+    public Game getGamesById(@PathVariable int id){
+        Optional<Game> returnValue = gameRepo.findById(id);
+        if (returnValue.isPresent()){
+            return returnValue.get();
+        } else{
+            return null;
+        }
+    }
+
+    @GetMapping("/games/rating/{esrbRating}")
+    public List<Game> getGamesByEsrbRating(@PathVariable String esrbRating) {
+        List<Game> returnVal = gameRepo.findAllGamesByEsrbRating(esrbRating);
+        if (returnVal.size() > 0) {
+            return returnVal;
+        } else {
+            return null;
+        }
+    }
+
+    @GetMapping("/games/title/{title}")
+    public List<Game> getGameByTitle(@PathVariable String title) {
+        List<Game> returnVal = gameRepo.findAllGamesByTitle(title);
+        if (returnVal.size() > 0) {
+            return returnVal;
+        } else {
+            return null;
+        }
+    }
+
+    @GetMapping("/games/studio/{studio}")
+    public List<Game> getGameByStudio(@PathVariable String studio) {
+        List<Game> returnVal = gameRepo.findAllGamesByStudio(studio);
+        if (returnVal.size() > 0) {
+            return returnVal;
+        } else {
+            return null;
+        }
+    }
+
+    @PostMapping("/games")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Game addGame(@RequestBody Game game) {
+        return gameRepo.save(game);
+    }
+
+    @PutMapping("/games")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateGame(@RequestBody Game game) {
+        gameRepo.save(game);
+    }
+
+    @DeleteMapping("/games/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteGame(@PathVariable int id) {
+        gameRepo.deleteById(id);
+    }
+
+
+
 }
--- src/main/java/com/company/FinalProjectSaraMorita/controllers/TshirtControllers.java
+++ src/main/java/com/company/FinalProjectSaraMorita/controllers/TshirtControllers.java
@@ -1,4 +1,72 @@
 package com.company.FinalProjectSaraMorita.controllers;
 
+import com.company.FinalProjectSaraMorita.models.Tshirt;
+import com.company.FinalProjectSaraMorita.repositories.TshirtRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
 public class TshirtControllers {
+    @Autowired
+    TshirtRepository tshirtRepository;
+
+    @GetMapping("/tshirts")
+    public List<Tshirt> getAllTshirts() {
+        return tshirtRepository.findAll();
+    }
+
+    public Tshirt frindTshirtById(@PathVariable int tShirtID){
+        Optional<Tshirt> returnValue = tshirtRepository.findById(tShirtID);
+
+        if (returnValue.isPresent()){
+            return returnValue.get();
+        }
+        else{
+            return null;
+        }
+    }
+
+    @GetMapping("/tshirts/size/{size}")
+    public List<Tshirt> getTshirtBySize(@PathVariable String size) {
+        List<Tshirt> returnVal = tshirtRepository.findTshirtBySize(size);
+        if(returnVal.size() > 0) {
+            return returnVal;
+        }
+        else{
+            return null;
+        }
+    }
+
+    @GetMapping("/tshirts/color/{color}")
+    public List<Tshirt> getTshirtByColor(@PathVariable String color) {
+        List<Tshirt> returnVal = tshirtRepository.findTshirtByColor(color);
+        if(returnVal.size() > 0) {
+            return returnVal;
+        }
+        else {
+            return null;
+        }
+    }
+
+    @PostMapping("/tshirts")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Tshirt addTshirt(@RequestBody Tshirt tshirt) {
+        return tshirtRepository.save(tshirt);
+    }
+
+    @PutMapping("/tshirts")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateTshirt(@RequestBody Tshirt tshirt) {
+        tshirtRepository.save(tshirt);
+    }
+
+    @DeleteMapping("/tshirts/{}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTshirt(@PathVariable int ID) {
+        tshirtRepository.deleteById(ID);
+    }
 }
